0bdf6dce16c364d586e70403760ba31a1c97cbfe
vsong2016 <vincent@linktime.cloud>
2023-09-23 22:00:38 +0800

    remove the test code

--- test.py
+++
@@ -1,9 +0,0 @@
-from langchain.document_loaders import JSONLoader
-
-loader = JSONLoader(
-    file_path='./data/guidelines.json',
-    jq_schema='.guidelines[].content',
-    text_content=False)
-
-data = loader.load()
-
