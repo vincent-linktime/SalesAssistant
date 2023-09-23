41cb9ccfe5573b3ba431cb88e7b4c0ba8230c8ed
vsong2016 <vincent@linktime.cloud>
2023-09-23 16:55:23 +0800

    use one prompt to detect both objections and accpetances

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
  This project aims to create a GPT-backed AI assistant for answering customer sales inquiries.
 
 ### Background
-The inspiration for this project stems from the [SalesCopilot](https://github.com/e-johnstonn/SalesCopilot) project. Our primary objective is to swiftly identify potential objections within customer inquiries and provide instant guidance on how to address them effectively. To streamline this process, we have omitted the call recording and transcription components from SalesCopilot, allowing us to enhance the assistant's capabilities beyond objection handling. In addition to objections, our assistant will also provide valuable recommendations, making it a comprehensive sales support tool.
-
-
+This project draws inspiration from the [SalesCopilot](https://github.com/e-johnstonn/SalesCopilot) project. Our primary goal is to promptly identify and address potential objections and acceptances in customer inquiries, offering instant guidance for effective responses. To streamline this, we've excluded call recording and transcription components from SalesCopilot, enabling us to enhance the assistant's capabilities for objection and acceptance handling.
--- src/prompts.py
+++ src/prompts.py
@@ -9,25 +9,25 @@ Keep responses concise and to the point. Starting now, answer the user's questio
 
 """
 
-DETECT_OBJECTION_PROMPT = """
-Your task is to read the question and discern whether the customer is raising any objections to the product or service we are selling.
-If the customer is simply stating their thoughts, preferences, or facts that are not specifically connected to the product or service, it is not an objection. 
+DETECT_ACCEPTANCE_OR_OBJECTION_PROMPT = """
+Your task is to read the question and discern whether the customer is showing any interests
+to the product or service we are selling, or whether the customer is raising any objections. 
+If the customer is simply stating their thoughts, preferences, or facts that are not specifically connected to the product or service, it is not a sign that they are interested nor it it is an objection. 
 Quote only from the question.
 Do not add, infer, or interpret anything.
-Example:
-'''
-Customer: I'm not sure if I can afford this. It's a bit expensive. The sky is blue. I like the color blue. 
 
-You: I'm not sure if I can afford this. It's a bit expensive.
-'''
-If there is no objection, respond with 'None'.
-Starting now, you will respond only with either the quote or None: 
-"""
+Acceptances sound like:
+'''What sets your product apart from X company's offerings?
+Could you highlight the key features of your product?
+What are the details of your pricing plans for the service?
+What is the typical ROI experienced by your current customers?
+Do you have any case studies or success stories related to your product?
+Is there a free trial or demo available for potential customers?
+What kind of post-purchase customer support do you provide?
+Are there integration options for your product with other software or systems we use?
+How frequently do you release updates or enhancements to your product?
+Could you share insights about your company's long-term vision or upcoming product developments?'''
 
-OBJECTION_GUIDELINES_PROMPT = """
-You are an assistant. You will be provided with a customer objection, a selection
-of guidelines on how to respond to certain objections, and the question from the customer. 
-Using the provided content, write out the response for the question.
 Objections sound like:
 '''It's too expensive.
 There's no money.
@@ -37,10 +37,32 @@ I don't want to get stuck in a contract.
 We're already working with another vendor.
 I'm locked into a contract with a competitor.
 I can get a cheaper version somewhere else.'''
+
+Example of acceptances:
+'''
+Customer: What your product or service can offer us. 
+
+You: I want to know more about your product and service.
+'''
+
+Example of objections:
+'''
+Customer: I'm not sure if I can afford this. It's a bit expensive. The sky is blue. I like the color blue. 
+
+You: I'm not sure if I can afford this. It's a bit expensive.
+'''
+If there is no interests or no objections, respond with 'None'.
+Starting now, you will respond only with either the quote or None: 
+"""
+
+GUIDELINES_PROMPT = """
+You are an assistant. You will be provided with a customer inquiry, a selection
+of guidelines on how to respond to certain inquiries. 
+Using the provided content, write out the response for the question.
  
 Example of your message:
 
-'It seems like you are {explain their objection}.
+'It seems like you are {explain their objection or interests}.
 
 {your response for the question}.'
 
